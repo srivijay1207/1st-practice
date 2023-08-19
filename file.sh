a67763d8b754cc10101dc13a63a96cbde5b85911
srivijay <srivijay1207@gmail.com>
2023-08-20 00:35:24 +0530

    excecutig

--- file.sh
+++ file.sh
@@ -5,6 +5,13 @@ USERID=$(id -u)
     echo "plss log in with root user"
     exit 1  
     fi
+    validate() {
+        if [ $1 -ne 0]; then
+        echo "instalation failure $2....."
+        else
+        echo "installation success $2......."
+        fi
+
     for i in $@
     do
     yum list installed $i
@@ -12,6 +19,7 @@ USERID=$(id -u)
     echo "already installed"
     else
     yum install $i
+    validate $? "done"
     echo "installation success"
     fi
     done
