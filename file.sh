a7157be8506d348303bdbf8cd8471049ac88cddc
srivijay <srivijay1207@gmail.com>
2023-08-20 00:24:24 +0530

    excecutig

--- file.sh
+++ file.sh
@@ -1,3 +1,15 @@
 
 #!/bin/bash
- "please welcome"
+USERID=$(id -u)
+    if [ $USERID -ne 0 ]; then
+    echo "plss log in with root user"
+    exit 1  
+    fi
+    for i in $@
+    do
+    yum list installed $i
+    if [ $? -ne 0]; then
+    echo "already installed"
+    else
+    yum install $i
+    echo "installation success"
